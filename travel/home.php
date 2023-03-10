931e092614d0d69dc18b242bc9c0c9a26cac800c
Nikita Yarosh <yarosh.nv@yandex.ru>
2023-03-10 11:25:28 +0300

    Add files via upload

---
+++ travel/home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Главная</title>
+        
+        <link rel=stylesheet href="https://cdnjs.cloudflare.com/ajax/libs/fork-awesome/1.2.0/css/fork-awesome.min.css">
+        
+        <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+        
+        <script src="js/script.js"></script>
+    </head>
+    <body>
+        
+        <!-- Header -->
+        
+        <header class="header">
+            
+            <a href="home.php" class="logo">Путешествия.</a>
+            
+            <nav class="navbar">
+                <a href="home.php">Главная</a>
+                <a href="about.php">Команда</a>
+                <a href="package.php">Услуги</a>
+                <a href="book.php">Забронировать</a>
+            </nav>
+            
+        </header>
+        
+        <!-- Footer -->
+        <footer class="footer">
+            <div class="box-container">
+                <div class="box">
+                    <h3>Страницы сайта</h3>
+                    <a href="home.php"> <i class="fa fa-angle-right"></i>Главная</a>
+                    <a href="about.php"><i class="fa fa-angle-right"></i>Команда</a>
+                    <a href="package.php"><i class="fa fa-angle-right"></i>Услуги</a>
+                    <a href="book.php"><i class="fa fa-angle-right"></i>Забронировать</a>
+                </div>
+                <div class="box">
+                    <h3>Контакты</h3>
+                    <a href="#"> <i class="fa fa-phone"></i>+123-456</a>
+                    <a href="#"><i class="fa fa-envelope"></i>tour@mail.com</a>
+                    <a href="#"><i class="fa fa-map"></i>Simferopol, Crimea</a>
+                </div>
+                <div class="box">
+                    <h3>Социальные сети</h3>
+                    <a href="#"> <i class="fa fa-vk"></i>vkontakte</a>
+                    <a href="#"><i class="fa fa-youtube-play"></i>youtube</a>
+                </div>
+            </div>
+            
+            <div class="credit">
+                
+                Создано <span>Мной</span> | все права защищены
+            </div>
+            
+        </footer>
+        
+        
+    </body>
+</html>
